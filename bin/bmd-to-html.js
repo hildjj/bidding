71e3bb1469319e743e4c41bae3350f0a0c359eed
Joe Hildebrand <joe-github@cursive.net>
2025-03-27 09:37:57 -0600

    Add nojekyll file

--- bin/bmd-to-html.js
+++ bin/bmd-to-html.js
@@ -7,25 +7,29 @@ import path from 'node:path';
 
 const srcs = process.argv.slice(2);
 
-fs.rmSync('docs', {recursive: true, force: true});
-fs.mkdirSync('docs');
+const docs = path.join(process.cwd(), 'docs');
+fs.rmSync(docs, {recursive: true, force: true});
+fs.mkdirSync(docs);
+fs.writeFileSync(path.join(docs, '.nojekyll'), '', 'utf8');
+
 for (const source of srcs) {
   if (source === 'README.md') {
     continue;
   }
   const text = fs.readFileSync(source, 'utf-8');
   const p = path.parse(source);
-  p.dir = path.join(p.dir, 'docs');
-  const outFile = path.format({
+  p.dir = docs;
+  const outFile = path.resolve(path.format({
     ...p,
     base: '',
     ext: '.htm',
-  });
+  }));
+  const outRel = path.relative(process.cwd(), outFile);
 
   try {
     const tree = bmd.parse(text, {grammarSource: source});
     fs.writeFileSync(outFile, tree.html(), 'utf8');
-    console.error(`${source} -> ${outFile}`);
+    console.error(`${source} -> ${outRel}`);
   } catch (e) {
     if (typeof e.format === 'function') {
       console.error(e.format([{source, text}]));
